5a60c1d381389bc5a99ed98ca7eb978c4e19f618
Viraj Dhanave <virajdhanave@gmail.com>
2026-06-14 13:35:30 +0530

    feat: add Whisper transcription

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, UploadFile, File
 import shutil
 import os
 
+from services.transcription import transcribe_audio
+
 router = APIRouter()
 
 UPLOAD_DIR = "uploads"
@@ -10,12 +12,16 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @router.post("/upload")
 async def upload_audio(file: UploadFile = File(...)):
+
     file_path = os.path.join(UPLOAD_DIR, file.filename)
 
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
+    transcript = transcribe_audio(file_path)
+
     return {
         "message": "Audio uploaded successfully",
-        "filename": file.filename
+        "filename": file.filename,
+        "transcript": transcript
     }
